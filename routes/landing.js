4537d582fff1aeca77dd6fef7857a980c4dc38ee
unknown <himanshupatel06@gmail.com>
2016-02-25 15:09:43 -0500

    All page working

--- routes/landing.js
+++ routes/landing.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET landing page. */
 router.get('/', function(req, res, next) {
   res.render('landing', { title: 'Express' });
+});
+
+/* GET people page. */
+router.get('/people', function(req, res) {
+  res.render('people', { title: 'People' });
+});
+
+/* GET people page. */
+router.get('/contact', function(req, res) {
+  res.render('contact', { title: 'Contact' });
 });
 
 module.exports = router;
